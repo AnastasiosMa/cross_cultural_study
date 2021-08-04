a9f58b3b5010e3466a57f244c6cba3c4d3605ecc
Martin Hartmann <martinarielhartmann@gmail.com>
2021-08-04 09:09:00 +0300

    some data explorations

---
+++ +stats/ancova_examples.m
@@ -0,0 +1,67 @@
+classdef ancova_examples < load_data.load_data
+%example: obj = stats.ancova_examples('responses_pilot/Music Listening Habits.csv','AllResponses'); do_ancova_examples(obj);
+
+    properties
+    end
+    methods
+        function obj = ancova_examples(dataPath,filterMethod)
+            if nargin < 2
+                error('ErrorTests:convertTest',...
+                      'Choose a filter method: \n  AllResponses \n  BalancedSubgroups');
+            end
+            if nargin == 0
+                dataPath = [];
+                filterMethod = [];
+            end
+            obj = obj@load_data.load_data(dataPath, filterMethod);
+        end
+        function obj = do_ancova_examples(obj)
+            close all
+            reasonLabels = {'for background purposes'
+                            'to bring up memories'
+                            'to have fun'
+                            'to feel music´s emotions'
+                            'to change your mood'
+                            'to express yourself'
+                            'to feel connected to other people'};
+            reasonTypes = {'General Behavior','Selected Track'};
+            % adding space before capital letters in variable names
+            filterMethod = regexprep(obj.filterMethod, '([A-Z])', ' $1');
+            for k = 1:numel(reasonTypes)
+                ReasonType = reasonTypes{k};
+                if matches(ReasonType,'General Behavior')
+                    tableFunctions = obj.dataTable(:,contains(obj.dataTable.Properties.VariableNames,'Music_'));
+                elseif  matches(ReasonType,'Selected Track')
+                    tableFunctions = obj.dataTable(:,contains(obj.dataTable.Properties.VariableNames,'Track_'));
+                end
+
+                Y = tableFunctions{:,:};
+                disp(['- Eliminating subjects with missing reason data (this problem applies to ''selected track'' only)'])
+                Y(any(isnan(tableFunctions{:,:}), 2), :) = [];% remove nan rows
+                set(0,'DefaultFigureVisible','off')
+                fa = stats.factor_analysis(obj.dataPath,obj.filterMethod);
+                set(0,'DefaultFigureVisible','on')
+                fa.FAscores(any(isnan(tableFunctions{:,:}), 2), :) = [];
+                Age = obj.dataTable.AgeCategory;
+                Age(any(isnan(tableFunctions{:,:}), 2), :) = [];
+
+                for j = 1:numel(reasonLabels)
+                    FactorNames = {'TendernessLove','TriumphBeauty','PainSadness','PleasureHappiness'};
+                    for i = 1:numel(FactorNames)
+                        disp(['- ' upper(ReasonType)])
+
+                        if matches(FactorNames{i},'TriumphBeauty')
+                            if matches(reasonLabels{j},'to have fun')
+                                if matches(ReasonType,'General Behavior')
+                                    [h,atab,ctab,stat] = aoctool(fa.FAscores(:,i),Y(:,j),Age,.05,FactorNames{i},reasonLabels{j},'Age');
+                                    multcompare(stat,0.05,'on','','s')
+                                end
+                            end
+                        end
+                        % to change your mood, tenderless/love also gives interesting results here
+                    end
+                end
+            end
+        end
+    end
+end
---
+++ +stats/explore_TIPI.m
@@ -0,0 +1,127 @@
+classdef explore_TIPI < load_data.load_data
+%example: obj = stats.explore_TIPI('responses_pilot/Music Listening Habits.csv','AllResponses');do_explore_TIPI(obj);
+
+    properties
+    end
+    methods
+        function obj = explore_TIPI(dataPath,filterMethod)
+            if nargin < 2
+                error('ErrorTests:convertTest',...
+                      'Choose a filter method: \n  AllResponses \n  BalancedSubgroups');
+            end
+            if nargin == 0
+                dataPath = [];
+                filterMethod = [];
+            end
+            obj = obj@load_data.load_data(dataPath, filterMethod);
+        end
+        function obj = do_explore_TIPI(obj)
+            obj.dataTable = removevars(obj.dataTable,{'RespondentID','Childhood','Adulthood','Residence','Identity','Duration'});
+            tipiCompleteLogical = ~any(isnan(obj.dataTable{:,matches(obj.dataTable.Properties.VariableNames,obj.TIPIscalesNames)}),2);
+            dataTableTIPIcomplete = obj.dataTable(tipiCompleteLogical,:);
+            S = vartype('numeric');
+            numericT = dataTableTIPIcomplete(:,S);
+            numericTnoNaNs = numericT(~any(isnan(numericT{:,:}),2),:);
+
+
+            c = corr(numericTnoNaNs{:,:});
+            c(triu(true(size(c)),1)) = NaN;
+            figure('units','normalized','outerposition',[0 0 1 1])
+            h = heatmap(c); % try to make it a heatmap so it's easier to navigate!
+            h.XDisplayLabels = strrep(numericTnoNaNs.Properties.VariableNames,'_',' ');
+            h.YDisplayLabels = strrep(numericTnoNaNs.Properties.VariableNames,'_',' ');
+
+            % show max highlights
+            tf = eye(size(c))==1;
+            c(tf) = NaN;
+            [m,I] = max(c,[],'Linear');
+            highlights = eye(size(c));
+            highlights(triu(true(size(highlights)),1)) = NaN;
+            highlights(I) = m;
+
+            figure('units','normalized','outerposition',[0 0 1 1])
+            h = heatmap(highlights); % try to make it a heatmap so it's easier to navigate!
+            h.XDisplayLabels = strrep(numericTnoNaNs.Properties.VariableNames,'_',' ');
+            h.YDisplayLabels = strrep(numericTnoNaNs.Properties.VariableNames,'_',' ');
+            title('max correlations')
+
+
+            % show min highlights
+            tf = eye(size(c))==1;
+            c(tf) = NaN;
+            [m,I] = min(c,[],'Linear');
+            highlights = eye(size(c));
+            highlights(triu(true(size(highlights)),1)) = NaN;
+            highlights(I) = m;
+
+            figure('units','normalized','outerposition',[0 0 1 1])
+            h = heatmap(highlights); % try to make it a heatmap so it's easier to navigate!
+            h.XDisplayLabels = strrep(numericTnoNaNs.Properties.VariableNames,'_',' ');
+            h.YDisplayLabels = strrep(numericTnoNaNs.Properties.VariableNames,'_',' ');
+            title('min correlations')
+        end
+    end
+    methods (Static)
+        function out = makestars(p)
+        % Create a cell array with stars from p-values (* p < .05; ** p < .01; *** p <
+        % .001). Input must be a matrix
+            names = {'','*', '**','***'};
+            stars = zeros(size(p));
+            stars(find(p < .001)) = 4;
+            stars(find(p >= .001 & p < .01)) = 3;
+            stars(find(p >= .01 & p < .05)) = 2;
+            stars(find(stars == 0)) = 1;
+            out = names(stars);
+        end
+        function t = makeTableWithVarNamesStars(r,p,VarNames,RowNames)
+        % formats into '%2.f',adds stars, makes varnames for
+        % columns and optionally rowNames
+            r = arrayfun(@(x) num2str(x,'%.2f'),r,'un',0);
+            % here we could also add the option of remove the 0
+            % before the decimal point
+            p = stats.explore_TIPI.makestars(p);
+            d = cellfun(@(x,y) join([x,y],'') ,r,p,'un',0);
+            if nargin == 3
+                t = array2table(string(d),'VariableNames',VarNames);
+            elseif nargin == 4
+                t = array2table(string(d),'VariableNames',VarNames,'RowNames',RowNames);
+            end
+        end
+        function tab = plotCorrMat(corrMat,pMat,ticklabels,myTitle)
+            set(groot,'defaultAxesTickLabelInterpreter','none');
+            corrMat(logical(triu(corrMat))) = NaN;
+            corrMat(1,:) = [];
+            corrMat(:,end) = [];
+            pMat(logical(triu(pMat))) = NaN;
+            pMat(1,:) = [];
+            pMat(:,end) = [];
+            [nr,nc] = size(corrMat);
+            p = pcolor([corrMat nan(nr,1); nan(1,nc+1)]);
+            shading faceted; % or flat
+            set(gca, 'ydir', 'reverse');
+            colorbar
+            xticks(1:numel(ticklabels));
+            yticks(1:numel(ticklabels));
+            xticks(xticks+.5);
+            yticks(yticks+.5);
+            xt = string(ticklabels(1:end-1));
+            xticklabels(xt)
+            yt = string(ticklabels(2:end));
+            yticklabels(yt(:))
+            xtickangle(45)
+            title(myTitle)
+            disp(myTitle)
+            tab = stats.explore_TIPI.makeTableWithVarNamesStars(corrMat,pMat,xt,yt);
+            disp(tab);
+        end
+        function savefigures(inputstring)
+        % get all figures saved as png and fig files. tikz and tex
+        % implementation is on its way?
+            h = get(0,'children');
+            for i=1:length(h)
+                saveas(h(i), [h(i).Name inputstring num2str(i) '.png'], 'png');
+                saveas(h(i), [h(i).Name inputstring num2str(i) '.fig'], 'fig');
+            end
+        end
+    end
+end
