5b21a7b7734f697f0708fa2f16b7c0da9a36d33f
Martin Hartmann <martinarielhartmann@gmail.com>
2021-09-20 12:03:10 +0300

    rename class about reasons

--- +stats/explore_reasons.m
+++ +stats/explore_reasons.m
@@ -1,21 +1,38 @@
-classdef predict_reasons_from_emotions < load_data.load_data
-%example: obj = stats.predict_reasons_from_emotions('responses_pilot/Music Listening Habits.csv','AllResponses'); do_predict_reasons_from_emotions(obj);
+classdef explore_reasons < load_data.load_data & stats.factor_analysis
+%example: obj = stats.explore_reasons(); obj.filterMethod='AllResponses';obj=do_load_data(obj);predict_reasons_from_emotions(obj);
+%obj = stats.explore_reasons(); obj.filterMethod='AllResponses';obj=do_load_data(obj);do_factor_analysis_reasons(obj)
 
     properties
     end
     methods
-        function obj = predict_reasons_from_emotions(dataPath,filterMethod)
-            if nargin < 2
-                error('ErrorTests:convertTest',...
-                      'Choose a filter method: \n  AllResponses \n  BalancedSubgroups');
-            end
-            if nargin == 0
-                dataPath = [];
-                filterMethod = [];
-            end
-            obj = obj@load_data.load_data(dataPath, filterMethod);
+        function obj = explore_reasons(obj)
+            obj=do_load_data(obj);
         end
-        function obj = do_predict_reasons_from_emotions(obj)
+        function obj = do_factor_analysis_reasons(obj)
+            reasonLabels = {'for background purposes'
+                            'to bring up memories'
+                            'to have fun'
+                            'to feel music´s emotions'
+                            'to change your mood'
+                            'to express yourself'
+                            'to feel connected to other people'};
+            reasonTypes = {'General Behavior','Selected Track'};
+            reasonTypesKey = {'Music_','Track_'};
+            obj.dataTable(any(ismissing(obj.dataTable{:,find(contains(obj.dataTable.Properties.VariableNames, reasonTypesKey))}),2),:) = [];
+            %figure('units','normalized','outerposition',[0 0 1 1])
+            for j = 1:numel(reasonTypesKey)
+                a = obj;
+                a.dataTableInd = find(contains(obj.dataTable.Properties.VariableNames, reasonTypesKey{j}));
+                a.dataTable.Properties.VariableNames(obj.dataTableInd) = strrep(obj.dataTable.Properties.VariableNames(obj.dataTableInd),'_',' ');
+                a.showPlotsAndText = 1;
+                a.showPlotsAndTextFA = 1;
+                a.removeLeastRatedTerms = 0;
+                a.rotateMethod = 'varimax';
+                a.PCNum =2;%number of factors
+                f(j) = do_factor_analysis(a);
+            end
+        end
+        function obj = predict_reasons_from_emotions(obj)
             close all
             reasonLabels = {'for background purposes'
                             'to bring up memories'
